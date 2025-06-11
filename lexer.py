18866840b8eecda64a6af87c1076eb15f26ee330
maria eduarda <mariaeduardalivros@gmail.com>
2025-06-11 20:04:27 -0300

    lexer gerando o lexer do obsact yupiii

--- lexer.py
+++ lexer.py
@@ -3,6 +3,8 @@ from ply.yacc import yacc
 
 file = open("c_file.txt", "w")
 
+__file__ = 'lexer.c'
+
 file.write(
     """
     #include <stdio.h>
@@ -11,7 +13,25 @@ file.write(
     """
 )
 
-reservados = {"ligar", "desligar", "enviar alerta", "dispositivo:", "dispositivos:", "se", "entao", "set", "observation", "senao", "&&", "."}
+reservados = (
+    "ligar", "desligar", "enviar_alerta", "dispositivo", "dispositivos", "se", "entao", "set", 
+    "observation", "senao", "and", "dot", "oplogic", "bool", "num", "namedevice", "namesensor"
+    )
+
+
+#expressões regulares dos tokens e constantes
+t_ligar         = r'ligar'
+t_desligar      = r'desligar'
+t_enviar_alerta = r'enviar alerta'
+t_dispositivo   = r'dispositivo:'
+t_dispositivos  = r'dispositivos:'
+t_se            = r'se'
+t_entao         = r'entao'
+t_set           = r'set'
+t_observation   = r'observation'
+t_senao         = r'senao'
+t_and           = r'&&'
+t_dot           = r'\.'
 
 def t_oplogic(t):
     r'<|>|>=|<=|==|!='
@@ -38,3 +58,12 @@ def t_namesensor(t):
     if t.value in reservados: 
         t.type = t.value
     return t
+
+t_ignore = " \t\n" #ignorar whitespaces
+
+def t_error(t):
+    print("Caracter ilegal: ", t.value[0])
+
+tokens = reservados #precisa de uma lista de tokens para rodar
+
+lexer = lex(debug=True)
